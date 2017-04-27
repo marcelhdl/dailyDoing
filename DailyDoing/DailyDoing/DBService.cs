4e72c2005510de4c8bb336fcc7b8fb44280a744d
MovieLol <martin@schulzegnt.de>
2017-04-27 19:52:36 +0200

    DBService: methods for lendings, comments
    Created the methods for the lending table (create,update,delete).
    Comment the specific methods.
    Adding/Rename regions.
    If-Else for MySQL-Connection Open/Close in every method.

--- DailyDoing/DailyDoing/DBService.cs
+++ DailyDoing/DailyDoing/DBService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+//using System.Data;
+//using System.Windows;
+//using System.Windows.Input;
 
 namespace DailyDoing
 {
@@ -105,14 +108,18 @@ namespace DailyDoing
         }
     #endregion
 
-    #region methods for database requests
+    #region methods for contacts requests
+
+        //Request all contacts for an specific user.
         public List<string[]> getContacts(MySqlConnection con, int uid)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "SELECT * FROM tbl_contacts WHERE uid='"+uid+"'";
+            command.CommandText = @"SELECT * 
+                                    FROM tbl_contacts 
+                                    WHERE uid='"+uid+"'";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
             List<string[]> contacts = new List<string[]>();
 
@@ -126,16 +133,20 @@ namespace DailyDoing
                 String[] contactInfo = row.Split(',');
                 contacts.Add(contactInfo);
             }
+            con.Close();
             return contacts;
         }
 
+        //Request the userID of a specific user.
         public int getUserID(MySqlConnection con, string username)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "SELECT uid FROM tbl_user WHERE username='" + username + "'";
+            command.CommandText = @"SELECT uid 
+                                    FROM tbl_user 
+                                    WHERE username='" + username + "'";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
             while (Reader.Read())
             {
@@ -144,17 +155,23 @@ namespace DailyDoing
                 {
                     row = Reader.GetValue(i).ToString();
                 }
+                con.Close();
                 return Convert.ToInt32(row);
             }
+            con.Close();
             return -1;
         }
+
+        //Request the details of a specific contact.
         public string[] getDetailsFromContacts(MySqlConnection con, int cid)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "SELECT * FROM tbl_contacts WHERE cid='" + cid + "'";
+            command.CommandText = @"SELECT * 
+                                    FROM tbl_contacts 
+                                    WHERE cid='" + cid + "'";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
             string[] contactinfo = new string[6];
 
@@ -166,18 +183,23 @@ namespace DailyDoing
                     row += Reader.GetValue(i).ToString() + ",";
                 }
                 contactinfo = row.Split(',');
+                con.Close();
                 return contactinfo;
             }
+            con.Close();
             return contactinfo;
         }
 
+        //Creates a contact for a specific user.
         public bool createContact(MySqlConnection con, int uid, string name, string vorname, string mail)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "INSERT INTO tbl_contacts (uid, name,vorname,mail) VALUES ('" + uid + "','" + name + "','" + vorname + "','" + mail + "')";
+            command.CommandText = @"INSERT INTO
+                                    tbl_contacts (uid, name,vorname,mail)
+                                    VALUES ('" + uid + "','" + name + "','" + vorname + "','" + mail + "')";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
 
             while (Reader.Read())
@@ -188,15 +210,20 @@ namespace DailyDoing
                     row = Reader.GetValue(i).ToString() + ",";
                 }
             }
+            con.Close();
             return true;
         }
+
+        //Updates an existing contact.
         public bool updateContact(MySqlConnection con, int cid, int uid, string name, string vorname, string mail)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "UPDATE tbl_contacts SET uid='" + uid + "', name='" + name + "', vorname='" + vorname + "', mail='" + mail + "' WHERE cid='" + cid +"'";
+            command.CommandText = @"UPDATE tbl_contacts 
+                                    SET uid='" + uid + "', name='" + name + "', vorname='" + vorname + "', mail='" + mail + 
+                                    "' WHERE cid='" + cid +"'";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
 
             while (Reader.Read())
@@ -207,15 +234,20 @@ namespace DailyDoing
                     row = Reader.GetValue(i).ToString() + ",";
                 }
             }
+            con.Close();
             return true;
         }
+
+        //Deletes a specific contact.
         public bool deleteContact(MySqlConnection con, int cid)
         {
             MySqlCommand command = con.CreateCommand();
-            command.CommandText = "DELETE FROM tbl_contacts WHERE cid='" + cid + "'";
+            command.CommandText = @"DELETE FROM 
+                                        tbl_contacts 
+                                        WHERE cid='" + cid + "'";
             MySqlDataReader Reader;
-            con.Close();
-            con.Open();
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
             Reader = command.ExecuteReader();
 
             while (Reader.Read())
@@ -226,8 +258,90 @@ namespace DailyDoing
                     row = Reader.GetValue(i).ToString() + ",";
                 }
             }
+            con.Close();
             return true;
         }
+
+
+        #endregion
+
+        #region methods for lending requests
+
+        // Create a Lending in the Database Table
+        // Please give DateTime.Date
+        public bool createLending(MySqlConnection con, int uid, int cid, string desc, DateTime date_lendback)
+        {
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = @"INSERT INTO
+                                    tbl_lendings
+                                    (uid, cid, desc, timestamp_lendback) 
+                                    VALUES
+                                    ('" + uid + "','" + cid + "','" + desc + "','" + date_lendback + "')";
+            MySqlDataReader Reader;
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
+            Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                {
+                    row = Reader.GetValue(i).ToString() + ",";
+                }
+            }
+            con.Close();
+            return true;
+        }
+
+        //Update a specific landing.
+        public bool updateLending(MySqlConnection con, int lid, int cid, string desc, DateTime date_lendback)
+        {
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = @"UPDATE tbl_contacts 
+                                    SET cid='" + cid + "', desc='" + desc + "', timestamp_lendback='" + date_lendback + 
+                                    "' WHERE lid='" + lid + "'";
+            MySqlDataReader Reader;
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
+            Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                {
+                    row = Reader.GetValue(i).ToString() + ",";
+                }
+            }
+            con.Close();
+            return true;
+        }
+
+        //Delete a specific landing.
+        public bool deleteLending(MySqlConnection con, int lid)
+        {
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = @"DELETE FROM 
+                                        tbl_lendings 
+                                        WHERE lid='" + lid + "'";
+            MySqlDataReader Reader;
+            if (con.State.ToString() == "Open") { }
+            else { con.Open(); }
+            Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                {
+                    row = Reader.GetValue(i).ToString() + ",";
+                }
+            }
+            con.Close();
+            return true;
+        }
+
 
 
         #endregion
