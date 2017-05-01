d75fedfcfbe46d5e83ffa4c20a23c7c5181640b2
MovieLol <martin@schulzegnt.de>
2017-05-01 18:01:38 +0200

    getter/setter Lendings modified

--- DailyDoing/DailyDoing/Lending.cs
+++ DailyDoing/DailyDoing/Lending.cs
@@ -19,17 +19,138 @@ namespace DailyDoing
         string timestamp_lendback;
         bool getback;
 
+        public int Lid
+        {
+            get
+            {
+                return lid;
+            }
+
+            set
+            {
+                lid = value;
+            }
+        }
+
+        public int Cid
+        {
+            get
+            {
+                return cid;
+            }
+
+            set
+            {
+                cid = value;
+            }
+        }
+
+        public int Uid
+        {
+            get
+            {
+                return uid;
+            }
+
+            set
+            {
+                uid = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Desc
+        {
+            get
+            {
+                return desc;
+            }
+
+            set
+            {
+                desc = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+
+        public string Priority
+        {
+            get
+            {
+                return priority;
+            }
+
+            set
+            {
+                priority = value;
+            }
+        }
+
+        public string Timestamp_lend
+        {
+            get
+            {
+                return timestamp_lend;
+            }
+
+            set
+            {
+                timestamp_lend = value;
+            }
+        }
+
+        public string Timestamp_lendback
+        {
+            get
+            {
+                return timestamp_lendback;
+            }
+
+            set
+            {
+                timestamp_lendback = value;
+            }
+        }
+
+        public bool Getback
+        {
+            get
+            {
+                return getback;
+            }
+
+            set
+            {
+                getback = value;
+            }
+        }
+
         public Lending(){}
 
-        public int Lid { get => lid; set => lid = value; }
-        public int Cid { get => cid; set => cid = value; }
-        public int Uid { get => uid; set => uid = value; }
-        public string Desc { get => desc; set => desc = value; }
-        public string Category { get => category; set => category = value; }
-        public string Priority { get => priority; set => priority = value; }
-        public string Timestamp_lend { get => timestamp_lend; set => timestamp_lend = value; }
-        public string Timestamp_lendback { get => timestamp_lendback; set => timestamp_lendback = value; }
-        public bool Getback { get => getback; set => getback = value; }
-        public string Title { get => title; set => title = value; }
+
     }
 }
